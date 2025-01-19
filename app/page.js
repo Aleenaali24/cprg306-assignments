2947ecf3b50677daf586c44f00fefa4e2f79d4e7
Aleenaali24 <aleenakhanally24@gmail.com>
2025-01-19 00:07:14 -0700

    added week-2 and styling

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <div>
-      <h1>Page</h1>
-      <p>This is the page content.</p>
+    <div className='bg-black min-h-screen  text-white flex flex-col  p-6' >
+      <h1 className="text-4xl font-bold pt-20 hover:text-[#FE347E] ml-40">CPRG 306: Web Development 2 - Assignments</h1>
+      <p>
+        <Link href="week-2" className='hover:underline hover:text-[#FE347E] ml-40 pt-32'>Week 2 Assignment</Link>
+      </p>
     </div>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Aleena Ali Khan</h1>
+      <p>      
+        <Link href="https://github.com/Aleenaali24/cprg306-assignments.git">My Github Repository:https://github.com/Aleenaali24/cprg306-assignments.git</Link>
+      </p>
+     
+    </div>
+  );
+}
